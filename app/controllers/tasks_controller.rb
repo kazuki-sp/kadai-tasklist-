0ef3117d9c080d058af3861c8a8ac20cabcfe898
kazuki.umemura <spboykazuki48@outlook.jp>
2020-03-02 21:12:31 +0000

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,56 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] =  '正常に登録できました'
+      redirect_to @task
+    else
+      flash.now[:danger] = '登録できませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = '正常に更新できました'
+      redirect_to @task
+    else
+      flash[:danger] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = '正常に削除できました'  
+    redirect_to @tasks_url
+  end
+  
+  private
+  #Strong Parameter
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
